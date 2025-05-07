df53750cde9f9a8eea96f40ae82056e2b4748597
Mariola Hullings <mariolahullings@gmail.com>
2025-05-06 21:08:22 -0400

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,3 @@
+// Imports the Sequelize class from the sequelize package.
+const { Sequelize } = require('sequelize'); // It helps you interact with your PostgreSQL database using JavaScript instead of raw SQL.
+require('dotenv').config()
